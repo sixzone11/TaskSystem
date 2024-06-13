f3039883c31bd4f69d8243e23d7504626ac61233
Jiman Jeong <sixzone11@gmail.com>
2024-06-14 02:22:19 +0900

    #11 보강 IS_TASK_SWITCH_ENABLED 추가 및 해당 flag가 비활성 상태일 때 Lambda Task에 반환 있는 경우의 대응 처리 추가.

--- TaskSystem/TaskCallableDescription.h
+++ TaskSystem/TaskCallableDescription.h
@@ -649,7 +649,7 @@ struct __Task_WaitWhile : __Task_Control {};
 
 struct __Task_SwitchDefault {};
 
-#define ProcessBlock(...)			[ __VA_ARGS__ ] (LambdaTaskIdentifier, auto info, auto&& resultTuple)
+#define ProcessBlock(...)			__task_expand_1(TASK_PROCESS_SWITCH, ProcessBlock, __VA_ARGS__)
 #define ConditionExpression(...)	[=] (LambdaTaskIdentifier, auto info, auto&& resultTuple) -> const bool { return (__VA_ARGS__) ; }
 #define Condition_Skip(...)			__Task_Condition{}, ConditionExpression(__VA_ARGS__)
 #define Condition_Cancel(...)		__Task_ConditionCancel{}, ConditionExpression(__VA_ARGS__)
@@ -675,33 +675,42 @@ struct __Task_SwitchDefault {};
 #define CommaConnection_Expand(N) CommaConnection_Select(N)
 #define CommaConnection(...) CommaConnection_Expand(IsEmptyArgs(__VA_ARGS__))
 
-#define TaskProcessBegin(task_name, ...)		__task_expand_0(TASK_PROCESS_SWITCH, TaskProcessBegin, task_name, __VA_ARGS__)
-#define TaskProcessNext(task_name, ...)			__task_expand_0(TASK_PROCESS_SWITCH, TaskProcessNext, task_name, __VA_ARGS__)
-#define TaskProcessEnd()						__task_expand_2(TASK_PROCESS_SWITCH, TaskProcessEnd )
+#define TaskProcessBegin(task_name, ...)					__task_expand_0(TASK_PROCESS_SWITCH, TaskProcessBegin, task_name, __VA_ARGS__)
+#define TaskProcessNext(task_name, ...)						__task_expand_0(TASK_PROCESS_SWITCH, TaskProcessNext, task_name, __VA_ARGS__)
+#define TaskProcessEnd()									__task_expand_2(TASK_PROCESS_SWITCH, TaskProcessEnd )
 
-#define Capture(...)							ProcessBlock(__VA_ARGS__)
-#define TaskProcessBeginCapture(task_name, ...)	__task_expand_0(TASK_PROCESS_SWITCH, TaskProcessBeginCapture, task_name, __VA_ARGS__)
-#define TaskProcessNextCapture(task_name, ...)	__task_expand_0(TASK_PROCESS_SWITCH, TaskProcessNextCapture, task_name, __VA_ARGS__)
+#define Capture(...)										ProcessBlock(__VA_ARGS__)
+#define TaskProcessBeginCapture(task_name, ...)				__task_expand_0(TASK_PROCESS_SWITCH, TaskProcessBeginCapture, task_name, __VA_ARGS__)
+#define TaskProcessNextCapture(task_name, ...)				__task_expand_0(TASK_PROCESS_SWITCH, TaskProcessNextCapture, task_name, __VA_ARGS__)
 
-#define create_task(...)						__task_expand_1(TASK_PROCESS_SWITCH, create_task, __VA_ARGS__)
+#define create_task(...)									__task_expand_1(TASK_PROCESS_SWITCH, create_task, __VA_ARGS__)
 
-#define TASK_PROCESS_SWITCH						__enable__
+#define TASK_PROCESS_SWITCH									__enable__
+//#define TASK_PROCESS_SWITCH								__disable__
+#define IS_TASK_SWITCH_ENABLED								__task_expand_2(TASK_PROCESS_SWITCH, TaskSwitch)
 
-#define __task_concat(x, y)						x##y
-#define __task_expand_0(x, y, task_name, ...)	__task_concat(x, y)(task_name, __VA_ARGS__)
-#define __task_expand_1(x, y, ...)				__task_concat(x, y)(__VA_ARGS__)
-#define __task_expand_2(x, y)					__task_concat(x, y)()
+#define __task_concat(x, y)									x##y
+#define __task_expand_0(x, y, task_name, ...)				__task_concat(x, y)(task_name, __VA_ARGS__)
+#define __task_expand_1(x, y, ...)							__task_concat(x, y)(__VA_ARGS__)
+#define __task_expand_2(x, y)								__task_concat(x, y)()
 
-#define __enable__TaskProcessBegin(task_name, ...)			auto task_name = Task(__VA_ARGS__ ProcessBlock()
-#define __enable__TaskProcessNext(task_name, ...)			); auto task_name = Task(__VA_ARGS__ ProcessBlock()
-#define __enable__TaskProcessEnd() )
-#define __enable__TaskProcessBeginCapture(task_name, ...)	auto task_name = Task(__VA_ARGS__
-#define __enable__TaskProcessNextCapture(task_name, ...)	); auto task_name = Task(__VA_ARGS__
+#define __enable__TaskSwitch()								1
+#define __enable__TaskProcessBegin(task_name, ...)			auto task_name = TaskWriter::task(__VA_ARGS__ ProcessBlock()
+#define __enable__TaskProcessNext(task_name, ...)			); auto task_name = TaskWriter::task(__VA_ARGS__ ProcessBlock()
+#define __enable__TaskProcessEnd()							)
+#define __enable__TaskProcessBeginCapture(task_name, ...)	auto task_name = TaskWriter::task(__VA_ARGS__
+#define __enable__TaskProcessNextCapture(task_name, ...)	); auto task_name = TaskWriter::task(__VA_ARGS__
 #define __enable__create_task(...)							ITaskManager::createTask(__VA_ARGS__)
 
-#define __disable__TaskProcessBegin(task_name, ...)
-#define __disable__TaskProcessNext(task_name, ...)
-#define __disable__TaskProcessEnd()
-#define __disable__TaskProcessBeginCapture(task_name, ...)
-#define __disable__TaskProcessNextCapture(task_name, ...)
-#define __disable__create_task(...)							(ITask*(nullptr))
+#define __enable__ProcessBlock(...)							[ __VA_ARGS__ ](LambdaTaskIdentifier, auto info, auto&& resultTuple)
+
+
+#define __disable__TaskSwitch()								0
+#define __disable__TaskProcessBegin(task_name, ...)			auto task_name = [&]()
+#define __disable__TaskProcessNext(task_name, ...)			(); auto task_name = [&]()
+#define __disable__TaskProcessEnd()							()
+#define __disable__TaskProcessBeginCapture(task_name, ...)	auto task_name = __VA_ARGS__
+#define __disable__TaskProcessNextCapture(task_name, ...)	(); auto task_name = __VA_ARGS__
+#define __disable__create_task(...)							((ITask*)(nullptr))
+
+#define __disable__ProcessBlock(...)						[ &, __VA_ARGS__ ]()
