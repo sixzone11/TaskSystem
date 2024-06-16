7c300809bbfe5dd664045508cce12abf1a2c9a97
jiman_jeong <sixzone11@gmail.com>
2024-06-17 02:21:05 +0900

    Thread Guard 테스트

--- Entrypoint/Entrypoint.cpp
+++ Entrypoint/Entrypoint.cpp
@@ -1,10 +1,12 @@
 ﻿#include "pch.h"
 
 void entrypoint_test();
+void threadguard_test();
 
 int main()
 {
 	std::cout << "Hello TaskSystem!" << std::endl;
 
 	entrypoint_test();
+	threadguard_test();
 }
