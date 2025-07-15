396188f6724efe8576bd61bba148e262c89451ea
Jiman Jeong <sixzone11@gmail.com>
2025-07-15 11:15:56 +0900

    c++ 20 이전 빌드를 위해 std::remove_cvref 추가

--- Entrypoint/pch.h
+++ Entrypoint/pch.h
@@ -8,3 +8,17 @@
 #include <tuple>
 #include <functional>
 #include <array>
+
+#if _MSVC_LANG < 202000L || __cplusplus < 202000L
+namespace std
+{
+	template<class T>
+	struct remove_cvref
+	{
+		using type = std::remove_cv_t<std::remove_reference_t<T>>;
+	};
+
+	template <class _Ty>
+	using remove_cvref_t = typename remove_cvref<_Ty>::type;
+}
+#endif
